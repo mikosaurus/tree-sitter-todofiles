c6ddfc8c45b44907d199ba0900fa96d391e0ca07
Anders Johannessen <mikosoft35@gmail.com>
2025-04-13 15:18:48 +0000

    First working parser with 0 errors

--- grammar.js
+++ grammar.js
@@ -11,7 +11,30 @@ module.exports = grammar({
   name: "todofiles",
 
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    source_file: $ => repeat($._definition),
+
+    _definition: $ => choice(
+      $.category_definition,
+      $.todo_definition,
+      $.started_definition,
+      $.done_definition,
+      $.cancelled_definition,
+      $.info_expression,
+    ),
+
+    category_definition: $ => $.category_expression,
+    todo_definition: $ => $.todo_expression,
+    started_definition: $ => $.started_expression,
+    done_definition: $ => $.done_expression,
+    cancelled_definition: $ => $.cancelled_expression,
+    info_definition: $ => $.info_expression,
+
+    category_expression: $ => token(seq(repeat(/\t/), /.*/, /:\n/)), 
+    todo_expression: $ => token(seq(repeat(/\t/), /\x{2610} /, /.*/, optional(/\n/))),
+    started_expression: $ => token(seq(repeat(/\t/), /\x{2610} /, /.*/, optional(/@started\(\d{2,4}.\d{2}.\d{2} \d{2}:\d{2}\)\n/))),
+    done_expression: $ => token(seq(repeat(/\t/), /\x{2714} /, /.*/, optional(/@done\(\d{2,4}.\d{2}.\d{2} \d{2}:\d{2}\)\n/))),
+    cancelled_expression: $ => token(seq(repeat(/\t/), /\x{2612} /, /.*/, optional(/@cancelled\(\d{2,4}.\d{2}.\d{2} \d{2}:\d{2}\)\n/))),
+    info_expression: $ => token(seq(repeat(/\t/), /.*/, /\n/)),
+
   }
 });
