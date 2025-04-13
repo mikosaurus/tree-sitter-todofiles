9b25c9213fb0be1320f8a9dc22b8c6d40c89f326
Anders Johannessen <mikosoft35@gmail.com>
2025-04-13 22:01:00 +0000

    Somewhat functioning grammar
    * not done any testing except for simple testing file

--- grammar.js
+++ grammar.js
@@ -9,45 +9,39 @@
 
 module.exports = grammar({
   name: "todofiles",
-  conflicts: $ => [
-    [$._category]
-  ],
 
   rules: {
     source_file: $ => repeat($._definition),
 
     _definition: $ => choice(
       $._category,
+      $.task,
     ),
 
     _category: $ => seq(
       $.category_definition,
-      repeat(choice($._category, $.data)),
     ),
 
     task: $ => choice(
       $.todo_definition,
-      $.started_definition,
       $.done_definition,
-      $.cancelled_definition
+      $.cancelled_definition,
+      $.comment,
     ),
 
-    info: $ => choice($.info_expression),
-    data: $ => choice($.task, $.info),
+    done_tag: $ => /@done\(\d{2,4}.\d{2}.\d{2} \d{2}:\d{2}\)\n/,
+    cancelled_tag: $ => /@cancelled\(\d{2,4}.\d{2}.\d{2} \d{2}:\d{2}\)\n/,
+    checkbox: $ => /\x{2610}/,
+    checkmark: $ => /\x{2714}/,
+    cancelled_mark: $ => /\x{2612}/,
 
-    category_definition: $ => $.category_expression,
-    todo_definition: $ => $.todo_expression,
-    started_definition: $ => $.started_expression,
-    done_definition: $ => $.done_expression,
-    cancelled_definition: $ => $.cancelled_expression,
-    info_definition: $ => $.info_expression,
+    category_definition: $ => seq(optional(repeat(/\t/)), $.category_expression,),
+    todo_definition: $ => seq(optional(repeat(/\t/)), $.checkbox, /.*/, /\n/),
+    done_definition: $ => seq(optional(repeat(/\t/)), $.checkmark, /.*/, $.done_tag),
+    cancelled_definition: $ => seq(optional(repeat(/\t/)), $.cancelled_mark, /.*/, $.cancelled_tag),
 
     category_expression: $ => token(seq(optional(repeat(/\t/)), /.*/, /:/, /\n/)), 
-    todo_expression: $ => token(seq(repeat(/\t/), /\x{2610} /, /.*/, optional(/\n/))),
-    started_expression: $ => token(seq(repeat(/\t/), /\x{2610} /, /.*/, optional(/@started\(\d{2,4}.\d{2}.\d{2} \d{2}:\d{2}\)\n/))),
-    done_expression: $ => token(seq(repeat(/\t/), /\x{2714} /, /.*/, optional(/@done\(\d{2,4}.\d{2}.\d{2} \d{2}:\d{2}\)\n/))),
-    cancelled_expression: $ => token(seq(repeat(/\t/), /\x{2612} /, /.*/, optional(/@cancelled\(\d{2,4}.\d{2}.\d{2} \d{2}:\d{2}\)\n/))),
-    info_expression: $ => token(seq(repeat(/\t/), /.*/,/\n/)),
+    comment:$ => seq(/#/, /.*/),
 
   }
 });
